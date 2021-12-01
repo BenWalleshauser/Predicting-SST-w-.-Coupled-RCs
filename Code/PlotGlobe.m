402838b806ef8ebeb7bc86a163f0cc4ea3f99bfd
Ben Walleshauser <72924413+BenWalleshauser@users.noreply.github.com>
2021-12-01 00:27:43 -0500

    Update PlotGlobe.m

--- Code/PlotGlobe.m
+++ Code/PlotGlobe.m
@@ -5,13 +5,13 @@ SSTPredicted_Plot = 250.*ones(n,m);
 SSTValidation_Plot = 250.*ones(n,m);
 
 SavePlot = 0;
-%Uncomment if you'd like to save the plot
-filename = ['C:\Users\benwa\Documents\Research\SST Predictor\Vids\',num2str(now*10^10),'.avi'];
-myVideo = VideoWriter(filename); %open video file
-myVideo.FrameRate = 10;  %can adjust this, 5 - 10 works well for me
-myVideo.Quality = 100;
-open(myVideo)
-SavePlot = 1;
+%Uncomment if you'd like to save the plot as an AVI
+%filename = ['MyVideo.avi'];
+%myVideo = VideoWriter(filename); %open video file
+%myVideo.FrameRate = 10;  %can adjust this, 5 - 10 works well for me
+%myVideo.Quality = 100;
+%open(myVideo)
+%SavePlot = 1;
 
 %Camera settings: 
 az = 50+180;
